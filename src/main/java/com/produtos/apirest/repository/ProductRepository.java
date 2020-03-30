ee7077da050c37fac4a758fc73f7a73b2fab8960
Spinkers <lucasmouraolopes@gmail.com>
2020-03-30 14:26:50 -0300

    refactor(api): Refatora o arquivo para ficar no padrão do ESlint

--- src/main/java/com/produtos/apirest/repository/ProductRepository.java
+++ src/main/java/com/produtos/apirest/repository/ProductRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.apirest.models.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
+
 }
